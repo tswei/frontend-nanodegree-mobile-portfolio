7a08427c008caaea29a966461ea9ae4f65cde1e3
tsimawei <turner.simawei@gmail.com>
2015-02-08 21:21:42 -0500

    Working Grunt js minifier

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,15 +7,36 @@ module.exports = function(grunt) {
 			options: {
 				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
-			build: {
-				src: 'src/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.name %>.min.js'
+			dynamic_mappings: {
+
+				files: [
+				{
+					expand: true,
+					cwd: '',
+					src: ['**/*.js', '!node_modules/**/*.js', '!Gruntfile.js'],
+					dest: '',
+					ext: '.min.js',
+					extDot: 'first'
+				},
+				],
+			}
+		},
+		jshint: {
+			dynamic_mappings: {
+				files: [
+				{
+					expand: true,
+					cwd: '',
+					src: ['**/*.js', '!node_modules/**/*.js', '!Gruntfile.js'],
+				},
+				],
 			}
 		}
 	});
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 
 	// Default task(s)
 	grunt.registerTask('default', ['uglify']);
